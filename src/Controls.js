4573d21b8c23c4d84c283214a731048f77cab266
jonashw <2jonwilson@gmail.com>
2014-07-06 03:04:34 -0700

    update for touch support / animated bg

--- src/Controls.js
+++ src/Controls.js
@@ -1,12 +1,113 @@
-var Controls = function(canvas,gameLoop,veggieLauncher){
+var Controls = function(canvas,stage,gameLoop){
 	var _obs = {
 		'swipestart':[],
 		'swipemove':[],
-		'swipestop':[]
+		'swipestop':[],
+		'combo':[],
+		'slice':[]
 	};
-	function notifyObs(evnt,x,y){
+	function notifyObs(evnt){
+		var args = [].slice.call(arguments,1);
 		_obs[evnt].forEach(function(ob){
-			ob(x,y);
+			ob.apply(null,args);
+		});
+	}
+	function setupSwipe(){
+		function getOffset(e){
+			if('originalEvent' in e && 'touches' in e.originalEvent){
+				var t = e.originalEvent.touches[0];
+				return {
+					x: t.pageX - t.target.offsetLeft,
+					y: t.pageY - t.target.offsetTop
+				};
+			} else {
+				return {
+					x: e.offsetX,
+					y: e.offsetY
+				};
+			}
+		}
+		var _mouseDown = false;
+		var _swipeStarted = false;
+		function startSwipe(e){
+			var o = getOffset(e);
+			notifyObs('swipestart',o.x,o.y);
+			_swipeStarted = true;
+			setTimeout(function(){
+				stopSwipe(e);
+			},300);
+		}
+		function stopSwipe(e){
+			_swipeStarted = false;
+			var o = getOffset(e);
+			notifyObs('swipestop',o.x,o.y);
+		}
+		function continueSwipe(e){
+			var o = getOffset(e);
+			notifyObs('swipemove',o.x,o.y);
+		}
+		//
+		var $canvas = $(canvas);
+		$canvas.on('mousedown touchstart',function(e){
+			e.preventDefault();
+			_mouseDown = true;
+			startSwipe(e);
+		});
+		$(document).on('mouseup touchend',function(e){
+			_mouseDown = false;
+			if(_swipeStarted){
+				stopSwipe(e);
+			}
+		});
+		$canvas.on('mousemove touchmove',function(e){
+			if(!_mouseDown){
+				return true;
+			}
+			if(_swipeStarted){
+				continueSwipe(e);
+			} else {
+				startSwipe(e);
+			}
+			e.stopPropagation();
+		});
+		document.body.addEventListener('mouseup touchend',function(e){
+			_mouseDown = false;
+		});
+	}
+	function setupCombos(self){
+		function centerOfVeggies(veggies){
+			if(veggies.length === 0){
+				return {x:0,y:0};
+			}
+			var sums = {x:0, y:0};
+			veggies.forEach(function(veggie){
+				sums.x += veggie.pos.x;
+				sums.y += veggie.pos.y;
+			});
+			return {
+				x: sums.x / veggies.length,
+				y: sums.y / veggies.length
+			};
+		}
+		var swipedVeggies = [];
+		self.on('swipestart swipemove',function(x,y){
+			var touchedVeggies = stage.getVeggiesAt(x, y);
+			touchedVeggies.forEach(function(veggie){
+				stage.splitVeggie(veggie);
+				if(swipedVeggies.indexOf(veggie) === -1){
+					swipedVeggies.push(veggie);
+				}
+				notifyObs('slice',veggie.type);
+			});
+			stage.swipeTrail.push({x:x,y:y});
+		});
+		self.on('swipestop',function(x,y){
+			stage.swipeTrail = [];
+			if(swipedVeggies.length > 2){
+				var center = centerOfVeggies(swipedVeggies);
+				notifyObs('combo',swipedVeggies.length,center.x,center.y);
+			}
+			swipedVeggies = [];
 		});
 	}
 	return {
@@ -18,52 +119,15 @@ var Controls = function(canvas,gameLoop,veggieLauncher){
 					}
 				}
 			});
-			var _mouseDown = false;
-			var _swipeStarted = false;
-			function startSwipe(e){
-				notifyObs('swipestart',e.offsetX,e.offsetY);
-				_swipeStarted = true;
-				setTimeout(function(e){
-					_swipeStarted = false;
-				},300);
+			setupSwipe();
+			if(!!stage){
+				setupCombos(this);
 			}
-			function stopSwipe(e){
-				notifyObs('swipestop',e.offsetX,e.offsetY);
-			}
-			function continueSwipe(e){
-				notifyObs('swipemove',e.offsetX,e.offsetY);
-			}
-
-			canvas.addEventListener('mousedown',function(e){
-				e.preventDefault();
-				_mouseDown = true;
-				startSwipe(e);
-			});
-			canvas.addEventListener('mouseup',function(e){
-				_mouseDown = false;
-				stopSwipe(e);
-			});
-			canvas.addEventListener('mousemove',function(e){
-				if(!_mouseDown){
-					return true;
-				}
-				if(_swipeStarted){
-					continueSwipe(e);
-				} else {
-					stopSwipe(e);
-					startSwipe(e);
-				}
-				e.stopPropagation();
-			});
-			document.body.addEventListener('mousemove',function(e){
-				if(_swipeStarted){
-					_mouseDown = false;
-					stopSwipe(e);
-				}
-			});
 		},
-		on: function(evnt,fn){
-			_obs[evnt].push(fn);
+		on: function(evnts,fn){
+			evnts.split(' ').forEach(function(evnt){
+				_obs[evnt].push(fn);
+			});
 		}
 	};
 };
--- src/Stage.js
+++ src/Stage.js
@@ -1,26 +1,71 @@
-var Stage = function(canvas,veggieImg,width,height){
+var Stage = function(canvas,veggieImg,width,height,bgImg){
 	var _img, _ctx;
+	var drawBg = (function(){
+		var bgRot = 1;
+		var s = 1000;
+		var ss = s / 1;
+		return function(){
+			if(!bgImg){
+				return;
+			}
+			_ctx.save();
+			_ctx.translate(s/4, s/4);
+			_ctx.rotate(Convert.degToRad(bgRot));
+			_ctx.drawImage(
+				bgImg,
+				0,
+				0,
+				s,
+				s,
+				ss/-2,
+				ss/-2,
+				ss,
+				ss);
+			_ctx.restore();
+			bgRot+=0.125;
+		};
+	})();
 	function veggieOffStage(veggie){
 		return veggie.pos.y < _ctx.canvas.height + 100;
 	}
 	function clear(){
-		_ctx.fillStyle = 'white';
-		_ctx.fillRect(0,0, _ctx.canvas.width, _ctx.canvas.height);
+		_ctx.clearRect(0,0, _ctx.canvas.width, _ctx.canvas.height);
 		_ctx.fillStyle = '#333';
 		_ctx.strokeRect(0,0, _ctx.canvas.width, _ctx.canvas.height);
+	}
+	function numDigits(n){
+		if(n == 0){
+			return 1;
+		} 
+		return Math.floor(Math.log(n) / Math.LN10) + 1;
+	}
+	function pairs(a){
+		if(a.length < 2){
+			return [];
+		} else {
+			var ps = []
+			var b = a.slice(1);
+			for(var i=0; i<b.length; i++){
+				ps.push({a: a[i], b: b[i]});
+			}
+			return ps;
+		}
 	}
 	return {
 		init: function(){
 			canvas.style.cursor = 'pointer';
 			_ctx = canvas.getContext('2d');
+			_ctx.imageSmoothingEnabled = false;
 			_ctx.canvas.width = width;
 			_ctx.canvas.height = height;
 			_img = veggieImg;
 			_scaleFactor = width / _img.width;
-			console.log('canvas width:',width,' image width:',_img.width, ' scale factor:',_scaleFactor);
+			//console.log('canvas width:',width,' image width:',_img.width, ' scale factor:',_scaleFactor);
 		},
 		veggies: [],
 		swipeTrail:[],
+		notices: [],
+		score: null,
 		getHeight: function(){
 			return _ctx.canvas.height;
 		},
@@ -32,6 +77,7 @@ var Stage = function(canvas,veggieImg,width,height){
 		},
 		draw: function(){
 			clear();
+			drawBg();
 			this.veggies.forEach(function(veggie,i){
 				veggie.draw(_img, _ctx, _scaleFactor);
 			});
@@ -39,13 +85,33 @@ var Stage = function(canvas,veggieImg,width,height){
 			this.veggies = this.veggies.filter(function(veggie){
 				return veggieOffStage(veggie);
 			});
-			this.swipeTrail.forEach(function(swipePoint){
-				_ctx.fillRect(swipePoint.x, swipePoint.y, 5, 5);
+			_ctx.save();
+			_ctx.strokeStyle = 'rgba(0,0,0,0.25)';//'rgba(15,175,15,0.5)';
+			_ctx.lineWidth = 5;
+			pairs(this.swipeTrail).forEach(function(pointPair){
+				_ctx.beginPath();
+				_ctx.moveTo(pointPair.a.x, pointPair.a.y);
+				_ctx.lineTo(pointPair.b.x, pointPair.b.y);
+				_ctx.stroke();
 			});
+			_ctx.fillStyle = 'rgba(0,0,0,0.5)';
+			this.notices.forEach(function(notice){
+				notice.draw(_ctx);
+			});
+			if(this.score != null){
+				_ctx.font = '40px Verdana';
+				var digits = numDigits(this.score);
+				var scoreX = canvas.width - (digits * 25) - 8;
+				_ctx.fillText(this.score, scoreX, 40);
+			}
+			_ctx.restore();
 		},
 		update: function(){
 			this.veggies.forEach(function(veggie,i){
 				veggie.update();
+			});
+			this.notices = this.notices.filter(function(notice){
+				return !notice.isExpired();
 			});
 		},
 		removeVeggie: function(veggie){
